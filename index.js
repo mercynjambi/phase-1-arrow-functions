40ce5212e37153c57098cb3a7bd4367722ab5ccc
mercynjambi <njambim695@gmail.com>
2024-03-28 23:00:25 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+ function divide(){
+    return 2000 / 100;
+ }
+ divide();
+
+ 
+const square = (num1, ) => num1 * num1;
+square(2);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(5, 6);
+
+
+
+
+ 
+
+ 
